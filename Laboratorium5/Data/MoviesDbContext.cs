bac44bd948095d430d68ee82da34c60f90c52086
Wojciech Walczuk <s95600@pollub.edu.pl>
2025-01-13 15:36:04 +0100

    Initial commit

---
+++ Laboratorium5/Data/MoviesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Laboratorium5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laboratorium5.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MoviesDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Laboratorium5/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Laboratorium5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [UIHint("LongText")]
+        public string Description { get; set; }
+        [UIHint("Stars")]
+
+        public int Rating { get; set; }
+        public string TrailerLink { get; set; }
+    }
+}
